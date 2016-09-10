68b3dc45cd7e66e4ad1a50435718859f4eb98245
Ben <Ben@BABYGIRL-THE-SE>
2016-09-10 16:30:59 -0700

    added file read capability and shader capability

--- TheEngine/TheEngine/main.cpp
+++ TheEngine/TheEngine/main.cpp
@@ -1,6 +1,7 @@
  #include "src/graphics/window.h"
 #include "src\maths\maths.h"
-
+//#include "src\utils\fileutils.h"
+#include "src\graphics\shader.h"
 
 
 int main()
@@ -9,27 +10,62 @@ int main()
 	using namespace graphics;
 	using namespace maths;
 
-	Window window("Sparky", 960, 540);
-	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+	Window window("Sparky", 960, 540);
+	//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+
+	GLfloat vertices[] = 
+	{
+		0,0,0,
+		8,0,0,
+		0,3,0,
+		0,3,0,
+		8,3,0,
+		8,0,0
+
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+
+
+	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
+	shader.enable();
+	shader.setUniformMat4("pr_matrix", ortho);
+	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
+
+	shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
+	shader.setUniform4f("colour", vec4(0.2f, 0.3f, 0.8f, 1.0f));
+
 
 	//**********TESTING**********
 	//std::cout << glGetString(GL_VERSION) << std::endl;
+
 	/*vec3 a(1.0f, 2.0f,5.0f);
+	std::string file = read_file("main.cpp");
+	std::cout << file << std::endl;
+	system("PAUSE");
+	return 0;
+
 	vec3 b(2,4,6);
 	vec3 c = a + b;
+
 	vec4 a(0.2f, 0.3f, 0.8f, 1.0f);
 	vec4 b(0.5f, 0.2f, 0.1f, 1.0f);
 	vec4 c = a + b;
+
+	mat4 position = mat4::translation(vec3(2, 3, 4));
+	position.multiply(mat4::scale(vec3(1,2,3)));
 	*/
 	//**************************
 
-	mat4 position = mat4::translation(vec3(2, 3, 4));
-	
-	position.multiply(mat4::scale(vec3(1,2,3)));
 
 	while (!window.closed())
 	{
@@ -47,21 +83,8 @@ int main()
 		{
 			exit(EXIT_FAILURE);
 		}
-		else if (window.isKeyPressed(GLFW_KEY_A))
-		{
-			std::cout << "The letter a was pressed" << std::endl;
-		}
-#if 1
-		glBegin(GL_QUADS);
-		glVertex2f(-.5f, -.5f);
-		glVertex2f(0.5f, -.5f);
-		glVertex2f(0.5f, .5f);
-		glVertex2f(-.5f, .5f);
-		glEnd();
-#else
-		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
 
-#endif		
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		window.update();
 	}
 
